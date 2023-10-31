9abc3f40db100deecff914561766ac14bc81537c
andre <andrepost@hotmail.se>
2023-10-31 19:16:23 +0100

    lab2BEN

--- lab2/Lab2/GensimW2V/main.py
+++ lab2/Lab2/GensimW2V/main.py
@@ -1,66 +1,72 @@
-# import modules & set up logging
 import gensim, logging, numpy as np
 import help_functions as hf
 import nltk
 
-#@author: The first version of this code is the courtesy of Vadim Selyanik
+# Define dimensions and repetitions
+dimensions = [10, 50, 100, 500, 1000]
+repetitions = 5
 
+# Download wordnet
+nltk.download('wordnet')
 
+# Set up logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-lemmatizer = nltk.WordNetLemmatizer() # create a lemmatizer
 
+lemmatizer = nltk.WordNetLemmatizer()
+
+# Load sentences from file
 sentences = []
-file = open("lemmatized.text", "r")
+with open("lemmatized.text", "r") as file:
+    for line in file:
+        sentences.append(line.split())
 
-for line in file: # read the file and create list which contains all sentences found in the text
-    sentences.append(line.split())
-# train word2vec on the two sentences
+# Define threshold
+threshold = 0.00055
 
-dimension = 50 # parameter for Word2vec size of vectors for word embedding
+for dimension in dimensions:
+    print(f"\n---- Running for dimension {dimension} ----\n")
+    for rep in range(repetitions):
+        print(f"\n--- Repetition {rep + 1} ---\n")
+        sum = 0.0
 
-threshold = 0.00055 # parameter for Word2vec
+        # Train model
+        model = gensim.models.Word2Vec(sentences, min_count=1, sample=threshold, sg=1, vector_size=dimension)
 
+        print(len(model.wv.key_to_index))
 
-sum = 0.0
+        # TOEFL tests
+        i = 0
+        number_of_tests = 80
+        right_answers = 0
+        number_skipped_tests = 0
+        with open('new_toefl.txt', 'r') as text_file:
+            while i < number_of_tests:
+                line = text_file.readline()
+                words = line.split()
+                try:
+                    words = [lemmatizer.lemmatize(lemmatizer.lemmatize(lemmatizer.lemmatize(word, 'v'), 'n'), 'a') for
+                             word in words]
+                    vectors = []
+                    if words[0] in model.wv:
+                        k = 1
+                        vectors.append(model.wv[words[0]])
+                        while k < 5:
+                            if words[k] in model.wv:
+                                vectors.append(model.wv[words[k]])
+                            else:
+                                vectors.append(np.random.randn(dimension))
+                            k += 1
+                        right_answers += hf.get_answer_mod(vectors)
+                except KeyError:
+                    number_skipped_tests += 1
+                    print("skipped test: " + str(i) + "; Line: " + str(words))
+                except IndexError:
+                    print(i)
+                    print(line)
+                    print(words)
+                    break
+                i += 1
 
-# 
-model = gensim.models.Word2Vec(sentences, min_count=1, sample=threshold, sg=1,size=dimension) # create model using Word2Ve with the given parameters
-#
-print(len(model.vocab)) # check the length of the vocabulary which was formed by Word2Vec
-
-#The rest implements passing TOEFL tests
-i = 0 #counter for TOEFL tests
-number_of_tests = 80
-text_file = open('new_toefl.txt', 'r')
-right_answers = 0 # variable for correct answers
-number_skipped_tests = 0 # some tests could be skipped if there are no corresponding words in the vocabulary extracted from the training corpus
-while i < number_of_tests:
-            line = text_file.readline() #read line in the file
-            words = line.split() # extract words from the line
-            try:
-                words = [lemmatizer.lemmatize(lemmatizer.lemmatize(lemmatizer.lemmatize(word, 'v'), 'n'), 'a') for word in
-                         words] # lemmatize words in the current test
-                vectors = []
-                if words[0] in model: # check if there embedding for the query word
-                    k = 1 #counter for loop iterating over 5 words in the test
-                    vectors.append(model[words[0]])
-                    while k < 5:
-                        if words[k] in model: # if alternative has the embedding
-                            vectors.append(model[words[k]]) #assing the learned vector
-                        else: 
-                            vectors.append(np.random.randn(dimension)) #assing random vector
-                        k += 1
-                    right_answers += hf.get_answer_mod(vectors) #find the closest vector and check if it is the correct answer
-
-            except KeyError: # if there is no representation for the query vector than skip
-                number_skipped_tests += 1
-                print("skipped test: " + str(i) + "; Line: " + str(words))
-            except IndexError:
-                print(i)
-                print(line)
-                print(words)
-                break
-            i += 1
-text_file.close()
-sum += 100 * float(right_answers) / float(number_of_tests) #get the percentage
-print("Threshold ferq = "+ str(threshold)+" Percentage of correct answers: " + str(sum) + "%")
+        sum += 100 * float(right_answers) / float(number_of_tests)
+        print("Dimension = " + str(dimension) + " Repetition = " + str(rep + 1) + " Threshold ferq = " + str(
+            threshold) + " Percentage of correct answers: " + str(sum) + "%")
